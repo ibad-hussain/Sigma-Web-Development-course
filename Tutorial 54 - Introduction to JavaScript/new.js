7a573a51afcf3a6561384dee0aa471b2ca53a0e1
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-06 07:49:14 +0500

    Added Tutorial 54

---
+++ Tutorial 54 - Introduction to JavaScript/new.js
@@ -0,0 +1,2 @@
+console.log("This is a code");
+console.log("This is another code");
---
+++ Tutorial 54 - Introduction to JavaScript/script.js
@@ -0,0 +1,15 @@
+alert("Hi this is google");
+
+console.log("This is a code");
+console.log("This is another code");
+
+var a = prompt("Enter a number")
+console.log("Your number is : " + a)
+
+var isTrue = confirm("Are you sure you want to leave this page?")
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
