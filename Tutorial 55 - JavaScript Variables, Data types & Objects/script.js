20cc3d85d1e3de9035eb28c6d80bbcb3790a9f1b
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-07 08:15:21 +0500

    Added Tutorial 55

---
+++ Tutorial 55 - JavaScript Variables, Data types & Objects/script.js
@@ -0,0 +1,51 @@
+console.log("This is tutorial 55");
+
+var a = 5;
+var b = 6;
+console.log(a+b)
+
+console.log(typeof a);
+
+let p = 2;
+if(a=5) {
+    let p = 60;
+    console.log(p);
+}
+console.log(p);
+
+var q = 10;
+var q = q+1;
+console.log(q)
+
+let r = 10;
+// let r = r+1;
+console.log(r)
+
+const s = 10;
+// const s = q+1;
+console.log(s)
+
+
+// DATA TYPES
+// Primitives
+let d = 23;
+let e = "ibad";
+let f = 3.22;
+const g = true;
+let h = undefined;
+let i = null;
+
+console.log(d, e, f, g, h, i);
+console.log(typeof d, typeof e, typeof f, typeof g, typeof h, typeof i);
+
+// Objects
+let o = {
+    name : "ibad",
+    "job code" : 254,
+    "is_handsome" : true
+}
+console.log(o)
+o.salary = 50000;
+console.log(o)
+o.salary = 80000;
+console.log(o)
