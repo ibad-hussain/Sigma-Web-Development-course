8669fc0b722a10d7972d6389bc5e62c54b481472
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-09 09:07:31 +0500

    Added Tutorial 57

---
+++ Tutorial 57 - JavaScript Loops/script.js
@@ -0,0 +1,65 @@
+console.log("Tutorial no 57");
+
+// for loop
+// let a = 1;
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(a+i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = a + i;
+//     console.log(element);
+// }
+
+
+
+// forin loop
+// let obj = {
+//     name : "ibad",
+//     role : "engineer",
+//     company : "systems"
+// }
+
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(element);
+// }
+
+// for (const key in obj) {
+//     console.log(obj[key]);
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element);
+// }
+
+
+
+// forof loop
+// for (const i of "ibad") {
+//     console.log(i)
+// }
+
+
+
+// while loop
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+
+
+// do while loop
+// let i = 11;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
