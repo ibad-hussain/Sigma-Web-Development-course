9b219d960a17ba7861cb6cb41940b9f9ead412b6
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-16 18:28:05 +0500

    Added Tutorial 63

---
+++ Tutorial 63 - JavaScript Arrays/script.js
@@ -0,0 +1,87 @@
+// node script.js
+
+// let arr =[1, 2, 3, 4, "hp", [2, 3]]
+// let arr =[2, 4, 12, 44, 8, 10]
+// let arr =[2, 4, 12, 44, 8]
+// console.log(arr)
+// console.log(arr[2])
+// console.log(typeof(arr))
+
+
+// Array Methods
+// console.log(arr.length)
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+// console.log(arr.push(30));
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.unshift("hp");
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+// delete(arr[2]);
+// console.log(arr);
+// let a = [1, 2, 3];
+// let b = [4, 5, 6];
+// let c = [7, 8, 9];
+// console.log(a.concat(b, c));
+// console.log(a.concat(b, c, "hp", 15));
+// let a = [11, 4, 7, 2, 22];
+// let a = ["z", "a", "t", "n", "d", "g"];
+// a.sort();
+// console.log(a);
+// arr.reverse();
+// console.log(arr);
+// arr.splice(2, 3, 555, 666, 777, 888);
+// console.log(arr);
+// console.log(arr.slice(2, 5));
+// console.log(arr);
+
+
+// Looping with Arrays
+// for loop
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// for each loop
+// arr.forEach(element => {
+//     console.log(element);
+// });
+// arr.forEach((value, index, array) => {
+//     console.log(value, index, array);
+// });
+
+// for of loop
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+
+
+// mfr methods
+// map
+// console.log(arr.map(e => {
+//     return e**2;
+// }));
+
+// filter
+// const greaterThanNine = (e) => {
+//     if (e>9) {
+//         return true;
+//     }
+//     return false;
+// }
+// console.log(arr.filter(greaterThanNine));
+
+// reduce
+// let arr2 = [1, 2, 3, 4, 5, 6];
+// const red = (a, b) => {
+//     return a+b;
+// };
+// console.log(arr2.reduce(red));
+
+
+// Array.from method
+// console.log(Array.from("ibad"));
