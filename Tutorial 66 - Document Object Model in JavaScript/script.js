f047a6f07d8ff572a227ed0adb56b43b5ad1e0d2
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-19 10:05:04 +0500

    Added Tutorial 66

---
+++ Tutorial 66 - Document Object Model in JavaScript/script.js
@@ -0,0 +1,9 @@
+window.console.log("hi")
+
+document.title = "ibad h"
+console.log(document.title);
+console.log(document.body);
+document.body.style.backgroundColor = "green"
+
+window.document
+window.document.body
