6cf505184c1206c7b4637c8351996a4d51d5a4bb
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-20 15:23:39 +0500

    Added Tutorial 67

---
+++ Tutorial 67 - Children, Parent & Siblings Nodes in DOM/script.js
@@ -0,0 +1,24 @@
+console.log(document.body.childNodes[1]);
+/*document.body.childNodes
+document.body.childNodes[1]
+document.body.childNodes[1].childNodes
+let cont = document.body.childNodes[1]
+cont.firstChild
+cont.lastChild
+cont.firstElementChild
+cont.lastElementChild
+cont.lastElementChild.style.backgroundColor = "red"
+cont.lastElementChild.parentElement
+document.body.firstElementChild.children
+document.body.firstElementChild.children[2]
+document.body.firstElementChild.children[2].previousSibling
+document.body.firstElementChild.children[2].nextSibling
+document.body.firstElementChild.children[2].previousElementSibling
+document.body.firstElementChild.children[2].nextElementSibling
+document.body.children[1]
+document.body.children[1].rows
+
+document.body.childNodes[1].hasChildNodes()     //output: true
+document.documentElement    //output: it gives content inside <html>
+document.head    //output: it gives content inside <head>  
+*/
