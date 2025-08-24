1e73b9dd9e92f86fd7c893c4189da8fa68c5dba4
ibad-hussain <ibadhussain2212@gmail.com>
2025-08-24 09:49:29 +0500

    Added Tutorial 71

---
+++ Tutorial 71 - Inserting & Removing Elements using JavaScript/Tutorial 71 copy/script.js
@@ -0,0 +1,35 @@
+// let new1 = document.createElement("div");
+// new1.innerHTML = "<b>I have been inserted</b>";
+// new1.setAttribute("class", "created");
+// document.querySelector(".container").append(new1);
+// document.querySelector(".container").prepend(new1);
+// document.querySelector(".container").before(new1);
+// document.querySelector(".container").after(new1);
+// document.querySelector(".container").replaceWith(new1);
+
+// let cont = document.querySelector(".container")
+// cont.insertAdjacentHTML("beforebegin", "<b>before begin</b>");
+// cont.insertAdjacentHTML("afterbegin", "<b>after begin</b>");
+// cont.insertAdjacentHTML("beforeend", "<b>before end</b>");
+// cont.insertAdjacentHTML("afterend", "<b>after end</b>");
+
+// document.querySelector(".box").remove()
+
+console.log(document.querySelector(".container").hidden);
+
+
+// let cont = document.querySelector(".container")
+// cont.insertAdjacentText("beforebegin", "before begin text");
+// cont.insertAdjacentText("afterbegin", "after begin text");
+// cont.insertAdjacentText("beforeend", "before end text");
+// cont.insertAdjacentText("afterend", "after end text");
+
+// let new1 = document.createElement("div");
+// new1.innerHTML = "<b>I have been inserted</b>";
+// new1.setAttribute("class", "created");
+// let cont = document.querySelector(".container")
+// cont.insertAdjacentElement("beforebegin", new1);
+// cont.insertAdjacentElement("afterbegin", new1);
+// cont.insertAdjacentElement("beforeend", new1);
+// cont.insertAdjacentElement("afterend", new1);
+
