e19b0d371aaf321970c3bc6a535d2817fbf07d01
ibad-hussain <ibadhussain2212@gmail.com>
2025-09-07 10:20:57 +0500

    Added Tutorial 85

---
+++ Tutorial 85 - Intro to Backend, NodeJS & npm/index.js
@@ -0,0 +1,7 @@
+// npm
+// Node.js
+// Modules
+// Packages
+// npm project initialization
+
+// Note: iIf node_modules folder is deleted, create it again by command "npm install". It will create the folder automatically by checking all the dependencies from the 'package.json' file.
