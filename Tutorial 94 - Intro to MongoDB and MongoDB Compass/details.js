88de95b153e510fba1dc6755fd9de1c69fe4e3b2
ibad-hussain <ibadhussain2212@gmail.com>
2025-09-19 09:38:37 +0500

    Added Tutorial 94

---
+++ Tutorial 94 - Intro to MongoDB and MongoDB Compass/details.js
@@ -0,0 +1,15 @@
+// mongodb installation
+
+// mongodb compass
+
+// mongodb VS code extension
+
+// mongosh (mongodb shell)
+
+// SOme mongosh commands:
+    // show databases
+    // use sigma
+    // db.courses.find()
+    // db.courses.insertOne({name: "Javascript"})
+
+// 'playground.mongodb.js' file
