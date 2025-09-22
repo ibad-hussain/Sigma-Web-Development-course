465e53c9f10ca713324c8f8a7e60b68bdbc69148
ibad-hussain <ibadhussain2212@gmail.com>
2025-09-22 10:53:30 +0500

    Added Tutorial 97

---
+++ Tutorial 97 - Exercise 16 (Dummy Data Generator)/details.js
@@ -0,0 +1,13 @@
+// Exercise 16 - Dummy Data Generator
+// Objective:
+    // Generate a dummy data in a collection () in a database ().
+    // The schema should be ;
+        // {
+        //      name: String,
+        //      salary: Number,
+        //      language: String,
+        //      city: String,
+        //      isManager: Bolean
+        // }
+    // Generate 10 such records when a button () is clicked.
+    // Create an Express app with mongoose to achieve it.
