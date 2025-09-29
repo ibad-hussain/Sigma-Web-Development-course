1d678684a8d3f0f6732dbe76a2b363cdfa8003e0
ibad-hussain <ibadhussain2212@gmail.com>
2025-09-29 10:05:29 +0500

    Added Tutorial 103

---
+++ Tutorial 103 - Hosting/details.js
@@ -0,0 +1,21 @@
+// Hosting
+
+// Credentials given and/or set when hosting is bought:
+    // ip
+    // username
+    // password
+
+// We can choose region of he server machine
+
+// Way to access server machine:
+    // SSH root@<ip>
+
+// Servers are Linux PCs
+
+// We can upload files using a software 'File Zilla'
+
+// Linux commands
+
+// Types of Hosting
+
+// Hosted HTML website on the server [on Hostinger]
