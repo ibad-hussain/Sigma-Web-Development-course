7bb543cc9f921ccb17da2618633e9339bee13259
ibad-hussain <ibadhussain2212@gmail.com>
2025-10-03 08:16:43 +0500

    Added Tutorial 121

---
+++ Tutorial 121 - Intro to NextJS & File-based Routing/details.js
@@ -0,0 +1,11 @@
+// Intro to Nextjs
+
+// File-based routing
+
+// App router vs Pages router
+
+// Features of Nextjs:
+    // Full stack solution
+    // File based routing
+    // Additional features like routing from next/navigation
+    // Optimized rendering
