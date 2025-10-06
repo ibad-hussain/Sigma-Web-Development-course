83afcae21122b7834b47098b66832e40bf13b18a
ibad-hussain <ibadhussain2212@gmail.com>
2025-10-06 12:33:26 +0500

    Added Tutorial 123

---
+++ Tutorial 123 - Link, Script & Image Components in NextJS/details.js
@@ -0,0 +1,6 @@
+// Link component
+    // For no reload
+// Script component
+    // To run extra JS script on rendering a page
+// Image component
+    // For image optimization, also configured 'next.config.mjs' for external image links
