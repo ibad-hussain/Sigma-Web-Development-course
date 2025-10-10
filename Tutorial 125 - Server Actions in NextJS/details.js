2fa09e549d5945ab9ac25f5bf93b5ab1fd0e5d6f
ibad-hussain <ibadhussain2212@gmail.com>
2025-10-10 17:54:22 +0500

    Added Tutorial 125

---
+++ Tutorial 125 - Server Actions in NextJS/details.js
@@ -0,0 +1,3 @@
+// Server actions in NextJS
+
+// Server actions/components vs API creation
---
+++ Tutorial 125 - Server Actions in NextJS/next-5/app/page.js
@@ -0,0 +1,25 @@
+"use client"
+import { submitAction } from "@/actions/form";
+import { useRef } from "react";
+
+export default function Home() {
+  let ref = useRef();
+
+  return (
+    <div>
+      <form ref={ref} action={(e)=> {submitAction(e); ref.current.reset()}}>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="add">Address: </label>
+          <input type="text" name="add" id="add" />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
